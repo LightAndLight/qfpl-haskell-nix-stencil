e7068f4067785a91060e06fdbf298722148a533a
Isaac Elliott <isaace71295@gmail.com>
2018-04-20 14:49:40 +1000

    Added more GHC versions

--- ci/ci.nix
+++ ci/ci.nix
@@ -1,6 +1,16 @@
 {
-  build =
+  build_ghc7103 =
     { system ? builtins.currentSystem }:
     let nixpkgs = import <nixpkgs> { inherit system; }; in
-    import ../default.nix { inherit nixpkgs; };
+    import ../default.nix { inherit nixpkgs; compiler = "ghc7103"; };
+
+  build_ghc802 =
+    { system ? builtins.currentSystem }:
+    let nixpkgs = import <nixpkgs> { inherit system; }; in
+    import ../default.nix { inherit nixpkgs; compiler = "ghc802"; };
+
+  build_ghc822 =
+    { system ? builtins.currentSystem }:
+    let nixpkgs = import <nixpkgs> { inherit system; }; in
+    import ../default.nix { inherit nixpkgs; compiler = "ghc822"; };
 }
